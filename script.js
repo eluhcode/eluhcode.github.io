c5e145569e70883168574f152889481becdceb29
eluh <106113306+eluhcode@users.noreply.github.com>
2022-06-02 16:44:45 -0230

    Update script.js

--- script.js
+++ script.js
@@ -17,21 +17,3 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   });
-
-// Animate on Scroll for Welcome and Display Sections
-window.sr = ScrollReveal();
-
-sr.reveal('.animate-left', {
-    origin: 'left',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
-});
-
-sr.reveal('.animate-right', {
-    origin: 'right',
-    duration: 1000,
-    distance: '25rem',
-    delay: 600
-});
-
